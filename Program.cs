59d7beecac7261d55ba10458fa380699a0f7c97e
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-25 15:53:17 -0700

    Update

--- Program.cs
+++ Program.cs
@@ -10,34 +10,61 @@ namespace TechnicalSkillsMentorship
     {
         static void Main(string[] args)
         {
-            // _2_FindFactorial
-            Console.WriteLine("_2_FindFactorial");
-            var factorial = new _2_FindFactorial();
-            Console.WriteLine($"Factorial 0: {factorial.Factorial(0)}");
-            Console.WriteLine($"Factorial 1: {factorial.Factorial(1)}");
-            Console.WriteLine($"Factorial 2: {factorial.Factorial(2)}");
-            Console.WriteLine($"Factorial 5: {factorial.Factorial(5)}");
-            Console.WriteLine($"Factorial 12: {factorial.Factorial(12)}");
+            // _3_CalculateMinClockAngle
+            Console.WriteLine("_3_CalculateMinClockAngle");
+            var minAngle = new _3_CalculateMinClockAngle();
+            Console.WriteLine("0345");
+            Console.WriteLine($"Minimum angle: {minAngle.MinClockAngle(0345)}");
+            Console.WriteLine("0930");
+            Console.WriteLine($"Minimum angle: {minAngle.MinClockAngle(0930)}");
+            Console.WriteLine("0250");
+            Console.WriteLine($"Minimum angle: {minAngle.MinClockAngle(0250)}");
+            Console.WriteLine("0305");
+            Console.WriteLine($"Minimum angle: {minAngle.MinClockAngle(0305)}");
+            Console.WriteLine("1505");
+            Console.WriteLine($"Minimum angle: {minAngle.MinClockAngle(1505)}\n");
 
-            Console.WriteLine("\n\n");
+            // _3_CalculateMinClockAngle (Precise)
+            Console.WriteLine("_3_CalculateMinClockAngle (Precise)");
+            var preciseMinAngle = new _3_CalculateMinClockAngle();
+            Console.WriteLine("0345");
+            Console.WriteLine($"Minimum angle: {preciseMinAngle.PreciseMinClockAngle(0345)}");
+            Console.WriteLine("0930");
+            Console.WriteLine($"Minimum angle: {preciseMinAngle.PreciseMinClockAngle(0930)}");
+            Console.WriteLine("0250");
+            Console.WriteLine($"Minimum angle: {preciseMinAngle.PreciseMinClockAngle(0250)}");
+            Console.WriteLine("0305");
+            Console.WriteLine($"Minimum angle: {preciseMinAngle.PreciseMinClockAngle(0305)}");
+            Console.WriteLine("1505");
+            Console.WriteLine($"Minimum angle: {preciseMinAngle.PreciseMinClockAngle(1505)}");
 
-            // _2_FindFactorial (No Recursion)
-            Console.WriteLine("_2_FindFactorial (No Recursion)");
-            var factorialNoRecursion = new _2_FindFactorial();
-            Console.WriteLine($"Factorial 0: {factorialNoRecursion.FactorialNoRecursion(0)}");
-            Console.WriteLine($"Factorial 1: {factorialNoRecursion.FactorialNoRecursion(1)}");
-            Console.WriteLine($"Factorial 2: {factorialNoRecursion.FactorialNoRecursion(2)}");
-            Console.WriteLine($"Factorial 5: {factorialNoRecursion.FactorialNoRecursion(5)}");
-            Console.WriteLine($"Factorial 12: {factorialNoRecursion.FactorialNoRecursion(12)}");
 
-            Console.WriteLine("\n\n");
+            //// _2_FindFactorial
+            //Console.WriteLine("_2_FindFactorial");
+            //var factorial = new _2_FindFactorial();
+            //Console.WriteLine($"Factorial 0: {factorial.Factorial(0)}");
+            //Console.WriteLine($"Factorial 1: {factorial.Factorial(1)}");
+            //Console.WriteLine($"Factorial 2: {factorial.Factorial(2)}");
+            //Console.WriteLine($"Factorial 5: {factorial.Factorial(5)}");
+            //Console.WriteLine($"Factorial 12: {factorial.Factorial(12)}");
 
-            // _1_AddDigitsUntilSingle
-            Console.WriteLine("_1_AddDigitsUntilSingle");
-            var singleTotal = new _1_AddDigitsUntilSingle();
-            Console.WriteLine($"Result: {singleTotal.SingleTotal(38)}\n");
-            Console.WriteLine($"Result: {singleTotal.SingleTotal(1112)}\n");
-            Console.WriteLine($"Result: {singleTotal.SingleTotal(483647)}\n");
+
+            //// _2_FindFactorial (No Recursion)
+            //Console.WriteLine("_2_FindFactorial (No Recursion)");
+            //var factorialNoRecursion = new _2_FindFactorial();
+            //Console.WriteLine($"Factorial 0: {factorialNoRecursion.FactorialNoRecursion(0)}");
+            //Console.WriteLine($"Factorial 1: {factorialNoRecursion.FactorialNoRecursion(1)}");
+            //Console.WriteLine($"Factorial 2: {factorialNoRecursion.FactorialNoRecursion(2)}");
+            //Console.WriteLine($"Factorial 5: {factorialNoRecursion.FactorialNoRecursion(5)}");
+            //Console.WriteLine($"Factorial 12: {factorialNoRecursion.FactorialNoRecursion(12)}");
+
+
+            //// _1_AddDigitsUntilSingle
+            //Console.WriteLine("_1_AddDigitsUntilSingle");
+            //var singleTotal = new _1_AddDigitsUntilSingle();
+            //Console.WriteLine($"Result: {singleTotal.SingleTotal(38)}\n");
+            //Console.WriteLine($"Result: {singleTotal.SingleTotal(1112)}\n");
+            //Console.WriteLine($"Result: {singleTotal.SingleTotal(483647)}\n");
 
         } // Main method
 
